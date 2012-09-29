4592f17c76d64d39108a69354e8d8d9c964be429
Jonas Eriksson <zqad@acc.umu.se>
2012-09-29 12:19:11 +0200

    Add dynamic output formatter (no dyn opts yet)
    Add outf() and use it. Preparation to add a command line option
    for formatting the output.
    
    Also, output "," at end of line by default

--- mcsign.c
+++ mcsign.c
@@ -36,6 +36,9 @@
 #include "region.h"
 
 #define SIGN_TAG "#map"
+#define DEFAULT_OUTPUT_FORMAT "{ \"x\": \"%x\", \"y\": \"%y\", " \
+	"\"z\": \"%z\", \"msg\": \"%u %v %w (%x, %y, %z)\" },\n"
+#define OUTPUT_BUF_SIZE 1024 /* Write output in at most 1k-blocks */
 
 struct region_data {
 	char *json_path;
@@ -82,22 +85,166 @@ bool cnbt_map_sign(nbt_node *node, void *aux) {
 	return true;
 }
 
+static inline nbt_node *safe_nbt_find_by_name(nbt_node *start_node,
+		const char *name) {
+	nbt_node *found_node;
+
+	found_node = nbt_find_by_name(start_node, name);
+	if (found_node == NULL) {
+		ERR("Internal error while searching for node %s.", name);
+		exit(1);
+	}
+	
+	return found_node;
+}
+
+static inline void fetch_value_int(nbt_node *node, const char *name,
+		const int32_t **dst) {
+	if (*dst != NULL)
+		return;
+
+	*dst = &(safe_nbt_find_by_name(node, name)->payload.tag_int);
+}
+
+static inline void fetch_value_str(nbt_node *node, const char *name,
+		const char **dst) {
+	nbt_node *found_node;
+
+	if (*dst != NULL)
+		return;
+
+	*dst = safe_nbt_find_by_name(node, name)->payload.tag_string;
+}
+
+size_t outf(FILE *fp, const char *format, nbt_node *node) {
+	char buf[OUTPUT_BUF_SIZE];
+	int buf_left = OUTPUT_BUF_SIZE - 1;
+	size_t total = 0;
+	const char *fmt_it = format;
+	char *buf_it = buf;
+	int ret, handle_ret;
+
+	/* Caches */
+	const char *text1 = NULL;
+	const char *text2 = NULL;
+	const char *text3 = NULL;
+	const char *text4 = NULL;
+	const int32_t *x = NULL;
+	const int32_t *y = NULL;
+	const int32_t *z = NULL;
+
+	while (*fmt_it != 0) {
+		if (*fmt_it == '%') {
+			/* Format character */
+			fmt_it++;
+			handle_ret = 0;
+			switch (*fmt_it) {
+			case 'x':
+				fetch_value_int(node, "x", &x);
+				ret = snprintf(buf_it, buf_left, "%d", *x);
+				handle_ret = 1;
+				break;
+			case 'y':
+				fetch_value_int(node, "y", &y);
+				ret = snprintf(buf_it, buf_left, "%d", *y);
+				handle_ret = 1;
+				break;
+			case 'z':
+				fetch_value_int(node, "z", &z);
+				ret = snprintf(buf_it, buf_left, "%d", *z);
+				handle_ret = 1;
+				break;
+			case 't':
+				fetch_value_str(node, "Text1", &text1);
+				if (*text1 != 0) {
+					ret = snprintf(buf_it, buf_left, "%s",
+							text1);
+					handle_ret = 1;
+				}
+				break;
+			case 'u':
+				fetch_value_str(node, "Text2", &text2);
+				if (*text2 != 0) {
+					ret = snprintf(buf_it, buf_left, "%s",
+							text2);
+					handle_ret = 1;
+				}
+				break;
+			case 'v':
+				fetch_value_str(node, "Text3", &text3);
+				if (*text3 != 0) {
+					ret = snprintf(buf_it, buf_left, "%s",
+							text3);
+					handle_ret = 1;
+				}
+				break;
+			case 'w':
+				fetch_value_str(node, "Text4", &text4);
+				if (*text4 != 0) {
+					ret = snprintf(buf_it, buf_left, "%s",
+							*text4);
+					handle_ret = 1;
+				}
+				break;
+			case '%':
+				*buf_it = '%';
+				buf_it++;
+				buf_left--;
+			default:
+				ERR("Illegal format character: '%c'",
+						*fmt_it);
+				exit(1);
+			}
+
+			if (handle_ret) {
+				if (ret < buf_left) {
+					buf_it += ret;
+					buf_left -= ret;
+				}
+				else {
+					/* Size left in buffer was to small,
+					 * indicate that buffer should be
+					 * written and rewind format iterator */
+					buf_left = 0;
+					fmt_it -= 2;
+				}
+			}
+		}
+		else {
+			/* Normal character */
+			*buf_it = *fmt_it;
+			buf_it++;
+			buf_left--;
+		}
+		fmt_it++;
+
+		if (buf_left == 0) {
+			*buf_it = 0;
+			if (fputs(buf, fp) == EOF) {
+				ERR("Error while writing to file: %d", errno);
+				exit(1);
+			}
+			buf_it = buf;
+			buf_left = OUTPUT_BUF_SIZE - 1;
+		}
+	}
+
+	*buf_it = 0;
+	if (fputs(buf, fp) == EOF) {
+		ERR("Error while writing to file: %d", errno);
+		exit(1);
+	}
+
+	return total;
+}
+
 void queue_output(gpointer data, gpointer user_data) {
 	FILE *fp = (FILE *)user_data;
 	nbt_node *node = (nbt_node *)data;
 	int x, y, z;
 	char *text2, *text3, *text4;
 
-	x = nbt_find_by_name(node, "x")->payload.tag_int;
-	y = nbt_find_by_name(node, "y")->payload.tag_int;
-	z = nbt_find_by_name(node, "z")->payload.tag_int;
-	text2 = nbt_find_by_name(node, "Text2")->payload.tag_string;
-	text3 = nbt_find_by_name(node, "Text3")->payload.tag_string;
-	text4 = nbt_find_by_name(node, "Text4")->payload.tag_string;
-
-	fprintf(fp, "{ \"x\": \"%d\",  \"y\": \"%d\",  \"z\": \"%d\", "
-			"\"msg\": \"%s %s %s (%d, %d, %d)\" }\n",
-			x, y, z, text2, text3, text4, x, y, z);
+	outf(fp, DEFAULT_OUTPUT_FORMAT, node);
 
 	return;
 }
--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ fi
 egrep -o 'r\.-?[0-9]*\.-?[0-9]*\.' $changes | cut -d. -f 2,3 | tr . ' ' | "$MCSIGN_DIR/mcsign" "$WORLD_DIR" "$SIGNS"
 (
   echo "var markerData = ["
-  find "$SIGNS" -name 'signs.*.in' -print0 | xargs -0 sed 's/$/,/'
+  find "$SIGNS" -name 'signs.*.in' -print0 | xargs -0 cat
   echo "];"
 ) > "$DESTINATION.tmp"
 mv "$DESTINATION.tmp" "$DESTINATION"
